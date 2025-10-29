02fe0285b003ca3ef5b08e51432fc51417f23cc7
Musa <mseyidoglu026@gmail.com>
2025-10-30 00:29:33 +0300

    fix: remove cloudflare adapter for static export

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-    site: "https://taragay.space/",
-    integrations: [mdx(), sitemap()],
-    vite: { plugins: [tailwindcss()] },
-    adapter: cloudflare({
-    platformProxy: {
-        enabled: true,
-      },
-    }),
+  site: "https://taragay.space/",
+  integrations: [mdx(), sitemap()],
+  vite: { plugins: [tailwindcss()] },
+  output: "static", // ✅ statik export için
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/compile", // ✅ sharp hatasını da önler
+    },
+  },
 });
