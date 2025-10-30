9400e62c152db21378dbe2f92eb45b5474b50cba
Musa <mseyidoglu026@gmail.com>
2025-10-30 23:02:16 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
+// @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+
+import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://taragay.space/",
-  integrations: [mdx(), sitemap()],
-  vite: { plugins: [tailwindcss()] },
-  output: "static", // ✅ statik export için
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/compile", // ✅ sharp hatasını da önler
-    },
-  },
+    site: "https://taragay.space/",
+    integrations: [mdx(), sitemap()],
+    vite: { plugins: [tailwindcss()] },
+    adapter: cloudflare({
+    platformProxy: {
+        enabled: true,
+      },
+    }),
 });
