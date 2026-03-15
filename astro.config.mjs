fac27f0165b6cecba18356a755fefb27f8211db6
Enes Berber <66645533+yearbarber@users.noreply.github.com>
2026-03-14 17:01:13 -0800

    fix: cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import cloudflare from '@astrojs/cloudflare';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://taragay.space/",
-  adapter: cloudflare(),
   integrations: [mdx(), sitemap()],
   vite: { plugins: [tailwindcss()] },
-  output: "server", // ✅ statik export için
+  output: "static", // ✅ statik export için
   image: {
     service: {
       entrypoint: "astro/assets/services/compile", // ✅ sharp hatasını da önler
