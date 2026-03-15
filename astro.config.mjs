2417fce6cfaa1396736d1c99a0ddc61c946d8861
Enes Berber <66645533+yearbarber@users.noreply.github.com>
2026-03-14 16:55:15 -0800

    fix: set adapter as cloudflare()

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import cloudflare from '@astrojs/cloudflare';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://taragay.space/",
+  adapter: cloudflare(),
   integrations: [mdx(), sitemap()],
   vite: { plugins: [tailwindcss()] },
   output: "static", // ✅ statik export için
